2819dd69f606b9d770657af7f3bb4999b2bc4213
Mohit Sharma <sharmamohit17799@gmail.com>
2019-01-26 15:43:57 +0530

    This is the first commit

---
+++ Round 1/PRESENT Block Cipher(Cipher Generation).py
@@ -0,0 +1,263 @@
+
+import codecs
+class Present:
+
+        def __init__(self,key,rounds=2):
+                """Create a PRESENT cipher object
+
+                key:    the key as a 128-bit or 80-bit rawstring
+                rounds: the number of rounds as an integer, 32 by default
+                """
+                self.rounds = rounds
+                if len(key) * 8 == 80:
+                        self.roundkeys = generateRoundkeys80(string2number(key),self.rounds)
+                elif len(key) * 8 == 128:
+                        self.roundkeys = generateRoundkeys128(string2number(key),self.rounds)
+                else:
+                        raise ValueError( "Key must be a 128-bit or 80-bit rawstring")
+
+        def encrypt(self,block):
+                """Encrypt 1 block (8 bytes)
+
+                Input:  plaintext block as raw string
+                Output: ciphertext block as raw string
+                """
+                state = string2number(block)
+                for i in range (self.rounds-1):
+                        state = addRoundKey(state,self.roundkeys[i])
+                        state = sBoxLayer(state)
+                        state = pLayer(state)
+                cipher = addRoundKey(state,self.roundkeys[-1])
+                return number2string_N(cipher,8)
+
+        def decrypt(self,block):
+                """Decrypt 1 block (8 bytes)
+
+                Input:  ciphertext block as raw string
+                Output: plaintext block as raw string
+                """
+                state = string2number(block)
+                for i in range (self.rounds-1):
+                        state = addRoundKey(state,self.roundkeys[-i-1])
+                        state = pLayer_dec(state)
+                        state = sBoxLayer_dec(state)
+                decipher = addRoundKey(state,self.roundkeys[0])
+                return number2string_N(decipher,8)
+
+        def get_block_size(self):
+                return 8
+
+#        0   1   2   3   4   5   6   7   8   9   a   b   c   d   e   f
+Sbox= [0xc,0x5,0x6,0xb,0x9,0x0,0xa,0xd,0x3,0xe,0xf,0x8,0x4,0x7,0x1,0x2]
+# x 0 1 2 3 4 5 6 7 8 9 A B C D E F
+# S[x] C 5 6 B 9 0 A D 3 E F 8 4 7 1 2
+
+Sbox_inv = [Sbox.index(x) for x in range(16)]
+PBox = [0,16,32,48,1,17,33,49,2,18,34,50,3,19,35,51,
+        4,20,36,52,5,21,37,53,6,22,38,54,7,23,39,55,
+        8,24,40,56,9,25,41,57,10,26,42,58,11,27,43,59,
+        12,28,44,60,13,29,45,61,14,30,46,62,15,31,47,63]
+
+
+PBox_inv = [PBox.index(x) for x in range(64)]
+
+def generateRoundkeys80(key,rounds):
+        """Generate the roundkeys for a 80-bit key
+
+        Input:
+                key:    the key as a 80-bit integer
+                rounds: the number of rounds as an integer
+        Output: list of 64-bit roundkeys as integers"""
+        roundkeys = []
+        for i in range(1,rounds+1): # (K1 ... K32)
+                # rawkey: used in comments to show what happens at bitlevel
+                # rawKey[0:64]
+                roundkeys.append(key >>16)
+                #1. Shift
+                #rawKey[19:len(rawKey)]+rawKey[0:19]
+                key = ((key & (2**19-1)) << 61) + (key >> 19)
+                #2. SBox
+                #rawKey[76:80] = S(rawKey[76:80])
+                key = (Sbox[key >> 76] << 76)+(key & (2**76-1))
+                #3. Salt
+                #rawKey[15:20] ^ i
+                key ^= i << 15
+        return roundkeys
+
+def generateRoundkeys128(key,rounds):
+        """Generate the roundkeys for a 128-bit key
+
+        Input:
+                key:    the key as a 128-bit integer
+                rounds: the number of rounds as an integer
+        Output: list of 64-bit roundkeys as integers"""
+        roundkeys = []
+        for i in range(1,rounds+1): # (K1 ... K32)
+                # rawkey: used in comments to show what happens at bitlevel
+                roundkeys.append(key >>64)
+                #1. Shift
+                key = ((key & (2**67-1)) << 61) + (key >> 67)
+                #2. SBox
+                key = (Sbox[key >> 124] << 124)+(Sbox[(key >> 120) & 0xF] << 120)+(key & (2**120-1))
+                #3. Salt
+                #rawKey[62:67] ^ i
+                key ^= i << 62
+        return roundkeys
+
+def addRoundKey(state,roundkey):
+        return state ^ roundkey
+
+def sBoxLayer(state):
+        """SBox function for encryption
+
+        Input:  64-bit integer
+        Output: 64-bit integer"""
+
+        output = 0
+        for i in range(16):
+                output += Sbox[( state >> (i*4)) & 0xF] << (i*4)
+        return output
+
+def sBoxLayer_dec(state):
+        """Inverse SBox function for decryption
+
+        Input:  64-bit integer
+        Output: 64-bit integer"""
+        output = 0
+        for i in range(16):
+                output += Sbox_inv[( state >> (i*4)) & 0xF] << (i*4)
+        return output
+
+def pLayer(state):
+        """Permutation layer for encryption
+
+        Input:  64-bit integer
+        Output: 64-bit integer"""
+        output = 0
+        for i in range(64):
+                output += ((state >> i) & 0x01) << PBox[i]
+        return output
+
+def pLayer_dec(state):
+        """Permutation layer for decryption
+
+        Input:  64-bit integer
+        Output: 64-bit integer"""
+        output = 0
+        for i in range(64):
+                output += ((state >> i) & 0x01) << PBox_inv[i]
+        return output
+
+def string2number(i):
+    """ Convert a string to a number
+
+    Input: string (big-endian)
+    Output: long or integer
+    """
+    return int(codecs.encode(i))
+
+def number2string_N(i, N):
+    """Convert a number to a string of fixed size
+
+    i: long or integer
+    N: length of string
+    Output: string (big-endian)
+    """
+    s = '%0*x' % (N*2, i)
+    return codecs.decode(s,'hex')
+
+
+def bytes_to_int(bytes):
+        result = 0
+
+        for b in bytes:
+            result = result * 256 + int(b)
+
+        return result
+
+
+def Binary(n):
+     s = bin(n)
+
+     # removing "0b" prefix
+     s1 = s[2:].zfill(64)
+     s2 = str(s1)
+     return s2
+
+def binaryToDecimal(n):
+    return int(n,2)
+data = Present("0100101001")
+import string
+
+indata = []
+outdata = []
+# here is the integer
+# print(bin(10018325639476897698)[2:])
+import random
+for i in range(10000):
+ h = random.getrandbits(64)
+ b = Binary(h)
+ indata.insert(i,b)
+ # print(b)
+# binary of this integer is 1000101100001000001111100001011011111001001101010110011110100010 which is 64bits
+# this is how we will send the input
+ k = data.encrypt(str(int(b,2)))
+ d = bytes_to_int(k)
+ a = Binary(d)
+ outdata.insert(i,a)
+ # print(len(a))
+# # a is the output and is also 64 bits long
+# print(len("1000101100001000001111100001011011111001001101010110001001010101"))
+# # print(len(Binary(bytes_to_int(k))))
+#
+# one part has been completed
+# we have 2 arrays both of size 10000 and both contain a bit sequence of 64 bits.
+import numpy as np
+inmat = np.empty((10000,16))
+outmat = np.empty((10000,64))
+
+for i in range(10000):
+        m = 1
+        n = 5
+        for k in range(16):
+
+                part = indata[i][m:n]
+                part1 = binaryToDecimal(part)
+                inmat[i][k] = part1
+                m +=4
+                n +=4
+
+
+# for q in range(10000):
+# for w in range(16):
+# print(inmat[q][w])
+# for i in range(10000):
+#     print(outdata[i])
+
+for i in range(10000):
+        m = 1
+        n = 2
+        for k in range(64):
+
+                part = outdata[i][m:n]
+                # part1 = binaryToDecimal(part)
+                if(part == ''):
+                    continue
+                # print(part)
+                outmat[i][k] = part
+                m +=1
+                n +=1
+
+
+# for q in range(10000):
+#   for w in range(64):
+#      print(outmat[q][w])
+
+# now creating csv files
+import csv
+with open("Input_features.csv","w+") as my_csv:
+    csvWriter = csv.writer(my_csv,delimiter=',')
+    csvWriter.writerows(inmat)
+with open("Label.csv","w+") as my_csv:
+    csvWriter = csv.writer(my_csv,delimiter=',')
+    csvWriter.writerows(outmat)
